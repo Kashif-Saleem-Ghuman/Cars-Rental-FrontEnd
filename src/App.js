be672f458468ee363852a42bb9860af70dd845b0
kashif <kashif_saleem_23@hotmail.com>
2023-10-17 07:06:58 +0500

    Refac: Uncommented home page route

--- src/App.js
+++ src/App.js
@@ -1,95 +1,87 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Homepage from './pages/homepage';
-import Reservationpage from './pages/reservationpage';
-import Detailspage from './pages/detailspage';
-import Reservations from './components/Reservations/Reservations';
-import MyReservations from './components/Reservations/MyReservations';
-import './App.css';
-import Reservationshistory from './pages/reservationshistory';
-import LoginPage from './pages/LoginPage';
-import Error from './pages/Error';
-import ProtectedRoute from './pages/ProtectedRoute';
-import { fetchCars } from './redux/slices/carSlice';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Homepage from "./pages/homepage";
+import Reservationpage from "./pages/reservationpage";
+import Detailspage from "./pages/detailspage";
+import Reservations from "./components/Reservations/Reservations";
+import MyReservations from "./components/Reservations/MyReservations";
+import "./App.css";
+import Reservationshistory from "./pages/reservationshistory";
+import LoginPage from "./pages/LoginPage";
+import Error from "./pages/Error";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import { fetchCars } from "./redux/slices/carSlice";
 
 const App = () => {
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(fetchCars());
-	}, []);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCars());
+  }, []);
 
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path="/*" element={<Error />} />
-					{/* <Route
-						path="/"
-						element={
-							<ProtectedRoute>
-								<Homepage />
-							</ProtectedRoute>
-						}
-					/> */}
-					<Route
-						path="/bookride"
-						element={
-							<ProtectedRoute>
-								<Reservations />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path="/addItem"
-						element={
-							<ProtectedRoute>
-								<Reservationpage />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path="/deleteItem"
-						element={
-							<ProtectedRoute>
-								<Reservationshistory />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path="/detailsPage/:id"
-						element={
-							<ProtectedRoute>
-								<Detailspage />
-							</ProtectedRoute>
-						}
-					/>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/*" element={<Error />} /> */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Homepage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/bookride"
+            element={
+              <ProtectedRoute>
+                <Reservations />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/addItem"
+            element={
+              <ProtectedRoute>
+                <Reservationpage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/deleteItem"
+            element={
+              <ProtectedRoute>
+                <Reservationshistory />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/detailsPage/:id"
+            element={
+              <ProtectedRoute>
+                <Detailspage />
+              </ProtectedRoute>
+            }
+          />
 
-					<Route
-						path="/myreservations"
-						element={
-							<ProtectedRoute>
-								<MyReservations />
-							</ProtectedRoute>
-						}
-					/>
-					<Route path="/register" element={<LoginPage />} />
-					<Route path="/*" element={<Error />} />
-				</Routes>
-				<ToastContainer position="top-center" />
-			</BrowserRouter>
-		</div>
-	);
+          <Route
+            path="/myreservations"
+            element={
+              <ProtectedRoute>
+                <MyReservations />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/register" element={<LoginPage />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+        <ToastContainer position="top-center" />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
-
-
-
-
-
-
-
-
